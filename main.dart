fdb6f42bdfcb0701d4b146b829c2f5a18504b7a1
Seshanidezoysa <133556611+Seshanidezoysa@users.noreply.github.com>
2024-05-15 01:41:09 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,80 @@
+import 'dart:async';
+
+import 'package:bus_booking_app/l10n/l10n.dart';
+import 'package:bus_booking_app/wrapper.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+import 'package:permission_handler/permission_handler.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize the time zone data
+  tz.initializeTimeZones();
+  // Set the default location
+  tz.setLocalLocation(tz.getLocation('Asia/Colombo'));
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+  const AndroidInitializationSettings initializationSettingsAndroid =
+  AndroidInitializationSettings('@mipmap/ic_launcher');
+  final InitializationSettings initializationSettings =
+  InitializationSettings(android: initializationSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<MyAppState>()!;
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  Locale _locale = const Locale('en', 'US');
+
+  void changeLanguage(String languageCode, String countryCode) {
+    setState(() {
+      _locale = Locale(languageCode, countryCode);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Permission.requestPermissions([Permission.location]);
+   Permission.location.request();
+    return MaterialApp(
+      title: 'Bus Finder App',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.blueAccent,
+        scaffoldBackgroundColor: Colors.grey[900],
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+          labelLarge: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+        ),
+      ),
+      supportedLocales: L10n.all,
+      locale: _locale,
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      home: Wrapper(changeLanguage: changeLanguage, locale: _locale),
+    );
+  }
+}
